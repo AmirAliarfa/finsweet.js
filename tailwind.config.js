6f253f9e4f6fe23dbd4c19d9ece8d94db4f0491b
unknown <you@example.com>
2023-11-10 18:40:39 +0330

    fineswet

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'tintblue':'rgba(28, 30, 83, 1)',
+        'bg-work':'#F4F6FC'
+      }
+    },
+  },
+  plugins: [],
+}
+
